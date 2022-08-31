c9babd1348717571d3cc7f161538edd35a0d708b
feather <Brian608@163.com>
2022-08-31 09:23:09 +0800

    消息发送 消费  存储

---
+++ demo04-rocketmq-producer/src/main/java/com/feather/rocketmq/demo/MyProducer.java
@@ -0,0 +1,11 @@
+/**
+ * @projectName:    rocketMQ 
+ * @package:        com.feather.rocketmq.demo
+ * @className:      MyProducer
+ * @author:     feather(杜雪松)
+ * @description:  TODO  
+ * @since:    2022/8/30 20:40
+ * @version:    1.0
+ */
+public class MyProducer {
+}
---
+++ demo05-rocketmq-comsumer/src/main/java/com/feather/rocketmq/consumer/MyConsumer.java
@@ -0,0 +1,11 @@
+/**
+ * @projectName:    rocketMQ 
+ * @package:        com.feather.rocketmq.consumer
+ * @className:      MyConsumer
+ * @author:     feather(杜雪松)
+ * @description:  TODO  
+ * @since:    2022/8/30 21:55
+ * @version:    1.0
+ */
+public class MyConsumer {
+}
